1574b3a9e5f0de7921c9e593f78b61d03498dd51
S-Sarvesh-Kumar <S.Sarvesh.Kumar@ibm.com>
2022-08-01 13:50:21 +0530

    remove layout not getting imported on vercel

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,20 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 import InputData from './api/sharedText/components/inputData'
-
 import OutputData from './api/sharedText/components/outputData'
 import EditData from './api/sharedText/components/editData'
 import DeleteData from './api/sharedText/components/deleteData'
-import Layout from './api/sharedText/components/Layout'
+
 
 export default function Home() {
   return (
-    <div className='bg'>
-      <Layout>
+    <div className='bg layout'>
+      
         <h1>Welcome to shareText</h1> <br></br>
         <InputData></InputData> <br></br>
         <OutputData></OutputData> <br></br>
         <EditData></EditData><br></br>
         <DeleteData></DeleteData><br></br>
-      </Layout>
     </div>
   )
 }
