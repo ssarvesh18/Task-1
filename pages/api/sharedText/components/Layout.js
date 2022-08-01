0c3b75407a9fa25c145f6e361e375fbb92458493
S-Sarvesh-Kumar <S.Sarvesh.Kumar@ibm.com>
2022-08-01 13:43:33 +0530

    style import changed

--- pages/api/sharedText/components/Layout.js
+++ pages/api/sharedText/components/Layout.js
@@ -3,7 +3,7 @@ import styles from '../../../../styles/layout.module.css'
 
 const Layout = ({children})=>{
     return (
-        <div className={styles.layout}>
+        <div className={styles.layout} >
             {children}
         </div>
      
--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,21 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 import InputData from './api/sharedText/components/inputData'
-import Layout from './api/sharedText/components/layout'
+
 import OutputData from './api/sharedText/components/outputData'
 import EditData from './api/sharedText/components/editData'
 import DeleteData from './api/sharedText/components/deleteData'
+import Layout from './api/sharedText/components/Layout'
 
 export default function Home() {
   return (
     <div className='bg'>
       <Layout>
-      <h1>Welcome to shareText</h1> <br></br>
-      <InputData></InputData> <br></br>
-      <OutputData></OutputData> <br></br>
-      <EditData></EditData><br></br>
-      <DeleteData></DeleteData><br></br>
+        <h1>Welcome to shareText</h1> <br></br>
+        <InputData></InputData> <br></br>
+        <OutputData></OutputData> <br></br>
+        <EditData></EditData><br></br>
+        <DeleteData></DeleteData><br></br>
       </Layout>
     </div>
   )
